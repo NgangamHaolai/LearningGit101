270bc33033b375cb86327b785fb2545908ce27b0
ngangam <ngangamhaolai100@gmail.com>
2025-12-04 11:38:34 +0530

    Jawan says Hello

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ package LearningGit101;
 
 public class Hello
 {
-
+    public static void main(String[] args)
+    {
+        System.out.println("I say Hello World... My name is Jawan and I am at your service");
+    }
 }
